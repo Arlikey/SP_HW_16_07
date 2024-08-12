ea65bc96080d49f5c1bec107da9c5a1de29e2903
admin <admin@DESKTOP-FA5KDQG>
2024-08-12 13:21:56 +0300

    Add project files.

---
+++ AdditionalTask1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP_HW_16_07
+{
+    internal class AdditionalTask1
+    {
+        static Random random = new Random();
+        static async Task Main(string[] args)
+        {
+            int[] array1 = new int[50];
+            int[] array2 = new int[50];
+
+            FillArrayRandom(array1);
+            FillArrayRandom(array2);
+
+            Console.WriteLine("Initial arrays:");
+
+            Console.Write("First array: ");
+            PrintArray(array1);
+
+            Console.Write("Second array: ");
+            PrintArray(array2);
+
+            var sortedArray1Task = BubbleSortAsync(array1);
+            var sortedArray2Task = InsertionSortAsync(array2);
+
+            var sortedArray1 = await sortedArray1Task;
+            var sortedArray2 = await sortedArray2Task;
+
+            Console.WriteLine("\nSorted arrays:");
+
+            Console.Write("First array: ");
+            PrintArray(sortedArray1);
+
+            Console.Write("Second array: ");
+            PrintArray(sortedArray2);
+        }
+        static void FillArrayRandom(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(100);
+            }
+        }
+        static async Task<int[]> BubbleSortAsync(int[] array)
+        {
+            return await Task.Run(() =>
+            {
+                BubbleSort(array);
+                return array;
+            });
+        }
+
+        static void BubbleSort(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                    }
+                }
+            }
+        }
+
+        static async Task<int[]> InsertionSortAsync(int[] array)
+        {
+            return await Task.Run(() =>
+            {
+                InsertionSort(array);
+                return array;
+            });
+        }
+
+        static void InsertionSort(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = key;
+            }
+        }
+
+        static void PrintArray(int[] array)
+        {
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿namespace SP_HW_16_07
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int[,] matrix1 = {
+                { 1, 2, 3 },
+                { 4, 5, 6 }
+            };
+
+            int[,] matrix2 = {
+                { 7, 8 },
+                { 9, 10 },
+                { 11, 12 }
+            };
+
+            int[,] result = await MultiplyMatricesAsync(matrix1, matrix2);
+
+            Console.WriteLine("Result:");
+            PrintMatrix(result);
+        }
+        static void PrintMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        static async Task<int[,]> MultiplyMatricesAsync(int[,] matrix1, int[,] matrix2)
+        {
+            int rows1 = matrix1.GetLength(0);
+            int cols1 = matrix1.GetLength(1);
+            int rows2 = matrix2.GetLength(0);
+            int cols2 = matrix2.GetLength(1);
+
+            if (cols1 != rows2)
+                throw new InvalidOperationException("Count of cols first matrix must be equal to count of rows second matrix.");
+
+            int[,] result = new int[rows1, cols2];
+
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < rows1; i++)
+                {
+                    for (int j = 0; j < cols2; j++)
+                    {
+                        int sum = 0;
+                        for (int k = 0; k < cols1; k++)
+                        {
+                            sum += matrix1[i, k] * matrix2[k, j];
+                        }
+                        result[i, j] = sum;
+                    }
+                }
+            });
+
+            return result;
+        }      
+    }
+}
